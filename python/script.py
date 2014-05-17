15b70ef433c33c97afa8ebaa8abf50c1f7c17398
Martijn Hiddink <support@mardinkwedesign.com>
2014-05-17 20:17:50 +0200

    Updated python script

--- python/script.py
+++ python/script.py
@@ -2,60 +2,93 @@ import webiopi
 import datetime
 import time
 import os
+import RPi.GPIO as GPIO
 
-GPIO = webiopi.GPIO
+#Define the ports
+Led_blue = 21 
+Led_red = 17
+Buzzer = 22
+Sonar = 14
+MotorA_dir = 7 #Rightside
+MotorA_speed = 8 #Rightside
+MotorB_dir = 9 #LeftSide
+MotorB_speed = 10 #LeftSide
 
-LIGHT = 17 # GPIO pin using BCM numbering
+#Set the servo port
+GPIO.setmode(GPIO.BCM) # Use Gpio pin numbering
+#Set ports OUT
+GPIO.setup(Led_red,GPIO.OUT) 
+GPIO.setup(Led_blue,GPIO.OUT)
+GPIO.setup(Buzzer,GPIO.OUT)
+GPIO.setup(MotorB_speed, GPIO.OUT)
+GPIO.setup(MotorB_dir, GPIO.OUT)
+GPIO.setup(MotorA_speed, GPIO.OUT)
+GPIO.setup(MotorA_dir, GPIO.OUT)
 
-HOUR_ON  = 8  # Turn Light ON at 08:00
-HOUR_OFF = 18 # Turn Light OFF at 18:00
 
-# setup function is automatically called at WebIOPi startup
-def setup():
-    # set the GPIO used by the light to output
-    GPIO.setFunction(LIGHT, GPIO.OUT)
-
-    # retrieve current datetime
-    now = datetime.datetime.now()
-
-    # test if we are between ON time and tun the light ON
-    if ((now.hour >= HOUR_ON) and (now.hour < HOUR_OFF)):
-        GPIO.digitalWrite(LIGHT, GPIO.HIGH)
-
-# loop function is repeatedly called by WebIOPi 
-def loop():
-    # retrieve current datetime
-    now = datetime.datetime.now()
-
-    # toggle light ON all days at the correct time
-    if ((now.hour == HOUR_ON) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT, GPIO.HIGH)
-
-    # toggle light OFF
-    if ((now.hour == HOUR_OFF) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT) == GPIO.HIGH):
-            GPIO.digitalWrite(LIGHT, GPIO.LOW)
-
-    # gives CPU some time before looping again
-    webiopi.sleep(1)
-
-# destroy function is called at WebIOPi shutdown
-def destroy():
-    GPIO.digitalWrite(LIGHT, GPIO.LOW)
-	
+#Functions Driving
 @webiopi.macro
-def getLightHours():
-    return "%d;%d" % (HOUR_ON, HOUR_OFF)
+def Stop():
+    GPIO.output (MotorA_speed,GPIO.LOW)
+    GPIO.output (MotorA_dir,GPIO.LOW)
+    GPIO.output (MotorB_speed,GPIO.LOW)
+    GPIO.output (MotorB_dir,GPIO.LOW)
+    
+@webiopi.macro
+def Forward():
+    # switch MotorA on forwards
+    GPIO.output (MotorA_speed, 1)
+    GPIO.output (MotorA_dir, 0)
+    # switch MotorB on forwards
+    GPIO.output (MotorB_speed, 1)
+    GPIO.output (MotorB_dir, 0)
+
+@webiopi.macro   
+def Forward_right():
+    # switch MotorA on backwards
+    GPIO.output (MotorA_speed, 0)
+    GPIO.output (MotorA_dir, 1)
+    # switch MotorB on forwards
+    GPIO.output (MotorB_speed, 1)
+    GPIO.output (MotorB_dir, 0)
+    
+@webiopi.macro
+def Forward_left():
+    # switch MotorA on forwards
+    GPIO.output (MotorA_speed, 1)
+    GPIO.output (MotorA_dir, 0)
+    # switch MotorB on backwards
+    GPIO.output (MotorB_speed, 0)
+    GPIO.output (MotorB_dir, 1)
 
 @webiopi.macro
-def setLightHours(on, off):
-    global HOUR_ON, HOUR_OFF
-    HOUR_ON = int(on)
-    HOUR_OFF = int(off)
-    return getLightHours()
+def Backwards():
+    # switch MotorA on backwards
+    GPIO.output (MotorA_speed, 0)
+    GPIO.output (MotorA_dir, 1)
+    # switch MotorB on backwards
+    GPIO.output (MotorB_speed, 0)
+    GPIO.output (MotorB_dir, 1)    
+    
+@webiopi.macro
+def Backwards_right():
+    # switch MotorA on forwards
+    GPIO.output (MotorA_speed, 1)
+    GPIO.output (MotorA_dir, 0)
+    # switch MotorB on backwards
+    GPIO.output (MotorB_speed, 0)
+    GPIO.output (MotorB_dir, 1)
+    
+@webiopi.macro
+def Backwards_left():
+    # switch MotorA on backwards
+    GPIO.output (MotorA_speed, 0)
+    GPIO.output (MotorA_dir, 1)
+    # switch MotorB on forwards
+    GPIO.output (MotorB_speed, 1)
+    GPIO.output (MotorB_dir, 0)
 
-#Function Pan and Tilt
+	#Function Pan and Tilt
 @webiopi.macro
 def pan_left():
     cmd = 'sudo echo 7=+10 > /dev/servoblaster'
@@ -95,3 +128,40 @@ def reboot():
 def shutdown():
     cmd = 'sudo shutdown -h now'
     os.system(cmd)
+
+#Function LEDS and Buzzer
+@webiopi.macro
+def LedBlue_on():
+    GPIO.output(Led_blue,GPIO.HIGH)
+
+@webiopi.macro
+def LedBlue_off():
+    GPIO.output(Led_blue,GPIO.LOW)
+
+@webiopi.macro
+def LedRed_on():
+    GPIO.output(Led_red,GPIO.HIGH)
+
+@webiopi.macro
+def LedRed_off():
+    GPIO.output(Led_red,GPIO.LOW)
+
+@webiopi.macro
+def Buzzer_on():
+    GPIO.output(Buzzer,GPIO.HIGH)
+
+@webiopi.macro
+def Buzzer_off():
+    GPIO.output(Buzzer,GPIO.LOW)
+   
+#Emergency function to stop all output   
+@webiopi.macro
+def Emergency():
+    GPIO.output(Buzzer,GPIO.LOW)
+    GPIO.output(Led_red,GPIO.LOW)
+    GPIO.output(Led_blue,GPIO.LOW)
+    GPIO.output (MotorA_speed,GPIO.LOW)
+    GPIO.output (MotorA_dir,GPIO.LOW)
+    GPIO.output (MotorB_speed,GPIO.LOW)
+    GPIO.output (MotorB_dir,GPIO.LOW)
+    GPIO.cleanup()
