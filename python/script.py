8d6fb0ce4e88b9ec545ce05d8d2f5973f0af63d0
Martijn Hiddink <support@mardinkwebdesign.com>
2014-05-20 07:40:31 +0200

    Rewritten to WebIOPI functions

--- python/script.py
+++ python/script.py
@@ -2,7 +2,8 @@ import webiopi
 import datetime
 import time
 import os
-import RPi.GPIO as GPIO
+
+GPIO = webiopi.GPIO
 
 #Define the ports
 Led_blue = 21 
@@ -14,79 +15,77 @@ MotorA_speed = 8 #Rightside
 MotorB_dir = 9 #LeftSide
 MotorB_speed = 10 #LeftSide
 
-#Set the servo port
-GPIO.setmode(GPIO.BCM) # Use Gpio pin numbering
-#Set ports OUT
-GPIO.setup(Led_red,GPIO.OUT) 
-GPIO.setup(Led_blue,GPIO.OUT)
-GPIO.setup(Buzzer,GPIO.OUT)
-GPIO.setup(MotorB_speed, GPIO.OUT)
-GPIO.setup(MotorB_dir, GPIO.OUT)
-GPIO.setup(MotorA_speed, GPIO.OUT)
-GPIO.setup(MotorA_dir, GPIO.OUT)
+def setup():
+	GPIO.setFunction(Led_red, GPIO.OUT)
+	GPIO.setFunction(Led_blue, GPIO.OUT)
+	GPIO.setFunction(Buzzer, GPIO.OUT)
+	GPIO.setFunction(MotorB_speed, GPIO.OUT)
+	GPIO.setFunction(MotorB_dir, GPIO.OUT)
+	GPIO.setFunction(MotorA_speed, GPIO.OUT)
+	GPIO.setFunction(MotorA_dir, GPIO.OUT)
 
 
 #Functions Driving
 @webiopi.macro
 def Stop():
-    GPIO.output (MotorA_speed,GPIO.LOW)
-    GPIO.output (MotorA_dir,GPIO.LOW)
-    GPIO.output (MotorB_speed,GPIO.LOW)
-    GPIO.output (MotorB_dir,GPIO.LOW)
+	GPIO.digitalWrite(MotorA_speed, GPIO.LOW)
+	GPIO.digitalWrite(MotorA_dir, GPIO.LOW)
+	GPIO.digitalWrite(MotorB_speed, GPIO.LOW)
+	GPIO.digitalWrite(MotorB_dir, GPIO.LOW)
     
 @webiopi.macro
 def Forward():
     # switch MotorA on forwards
-    GPIO.output (MotorA_speed, 1)
-    GPIO.output (MotorA_dir, 0)
+    GPIO.digitalWrite(MotorA_speed, GPIO.HIGH)
+    GPIO.digitalWrite(MotorA_dir, GPIO.LOW)
     # switch MotorB on forwards
-    GPIO.output (MotorB_speed, 1)
-    GPIO.output (MotorB_dir, 0)
+    GPIO.digitalWrite(MotorB_speed, GPIO.HIGH)
+    GPIO.digitalWrite(MotorB_dir, GPIO.LOW)
 
 @webiopi.macro   
 def Forward_right():
     # switch MotorA on backwards
-    GPIO.output (MotorA_speed, 0)
-    GPIO.output (MotorA_dir, 1)
+    GPIO.digitalWrite(MotorA_speed, GPIO.LOW)
+    GPIO.digitalWrite(MotorA_dir, GPIO.HIGH)
     # switch MotorB on forwards
-    GPIO.output (MotorB_speed, 1)
-    GPIO.output (MotorB_dir, 0)
+    GPIO.digitalWrite(MotorB_speed, GPIO.HIGH)
+    GPIO.digitalWrite(MotorB_dir, GPIO.LOW)
     
 @webiopi.macro
 def Forward_left():
     # switch MotorA on forwards
-    GPIO.output (MotorA_speed, 1)
-    GPIO.output (MotorA_dir, 0)
+    GPIO.digitalWrite(MotorA_speed, GPIO.HIGH)
+    GPIO.digitalWrite(MotorA_dir, GPIO.LOW)
     # switch MotorB on backwards
-    GPIO.output (MotorB_speed, 0)
-    GPIO.output (MotorB_dir, 1)
+    GPIO.digitalWrite(MotorB_speed, GPIO.LOW)
+    GPIO.digitalWrite(MotorB_dir, GPIO.HIGH)
 
 @webiopi.macro
 def Backwards():
     # switch MotorA on backwards
-    GPIO.output (MotorA_speed, 0)
-    GPIO.output (MotorA_dir, 1)
+    GPIO.digitalWrite(MotorA_speed, GPIO.LOW)
+    GPIO.digitalWrite(MotorA_dir, GPIO.HIGH)
     # switch MotorB on backwards
-    GPIO.output (MotorB_speed, 0)
-    GPIO.output (MotorB_dir, 1)    
+    GPIO.digitalWrite(MotorB_speed, GPIO.LOW)
+    GPIO.digitalWrite(MotorB_dir, GPIO.HIGH)    
     
 @webiopi.macro
 def Backwards_right():
     # switch MotorA on forwards
-    GPIO.output (MotorA_speed, 1)
-    GPIO.output (MotorA_dir, 0)
+    GPIO.digitalWrite(MotorA_speed, GPIO.HIGH)
+    GPIO.digitalWrite(MotorA_dir, GPIO.LOW)
     # switch MotorB on backwards
-    GPIO.output (MotorB_speed, 0)
-    GPIO.output (MotorB_dir, 1)
+    GPIO.digitalWrite(MotorB_speed, GPIO.LOW)
+    GPIO.digitalWrite(MotorB_dir, GPIO.HIGH)
     
 @webiopi.macro
 def Backwards_left():
     # switch MotorA on backwards
-    GPIO.output (MotorA_speed, 0)
-    GPIO.output (MotorA_dir, 1)
+    GPIO.digitalWrite(MotorA_speed, GPIO.LOW)
+    GPIO.digitalWrite(MotorA_dir, GPIO.HIGH)
     # switch MotorB on forwards
-    GPIO.output (MotorB_speed, 1)
-    GPIO.output (MotorB_dir, 0)
+    GPIO.digitalWrite(MotorB_speed, GPIO.HIGH)
+    GPIO.digitalWrite(MotorB_dir, GPIO.LOW)
 
 	#Function Pan and Tilt
 @webiopi.macro
@@ -132,36 +131,36 @@ def shutdown():
 #Function LEDS and Buzzer
 @webiopi.macro
 def LedBlue_on():
-    GPIO.output(Led_blue,GPIO.HIGH)
+    GPIO.digitalWrite(Led_blue,GPIO.HIGH)
 
 @webiopi.macro
 def LedBlue_off():
-    GPIO.output(Led_blue,GPIO.LOW)
+    GPIO.digitalWrite(Led_blue,GPIO.LOW)
 
 @webiopi.macro
 def LedRed_on():
-    GPIO.output(Led_red,GPIO.HIGH)
+    GPIO.digitalWrite(Led_red,GPIO.HIGH)
 
 @webiopi.macro
 def LedRed_off():
-    GPIO.output(Led_red,GPIO.LOW)
+    GPIO.digitalWrite(Led_red,GPIO.LOW)
 
 @webiopi.macro
 def Buzzer_on():
-    GPIO.output(Buzzer,GPIO.HIGH)
+    GPIO.digitalWrite(Buzzer,GPIO.HIGH)
 
 @webiopi.macro
 def Buzzer_off():
-    GPIO.output(Buzzer,GPIO.LOW)
+    GPIO.digitalWrite(Buzzer,GPIO.LOW)
    
-#Emergency function to stop all output   
-@webiopi.macro
-def Emergency():
-    GPIO.output(Buzzer,GPIO.LOW)
-    GPIO.output(Led_red,GPIO.LOW)
-    GPIO.output(Led_blue,GPIO.LOW)
-    GPIO.output (MotorA_speed,GPIO.LOW)
-    GPIO.output (MotorA_dir,GPIO.LOW)
-    GPIO.output (MotorB_speed,GPIO.LOW)
-    GPIO.output (MotorB_dir,GPIO.LOW)
+#When webIoPi shutsdown   
+
+def destroy():
+    GPIO.digitalWrite(Buzzer,GPIO.LOW)
+    GPIO.digitalWrite(Led_red,GPIO.LOW)
+    GPIO.digitalWrite(Led_blue,GPIO.LOW)
+    GPIO.digitalWrite(MotorA_speed,GPIO.LOW)
+    GPIO.digitalWrite(MotorA_dir,GPIO.LOW)
+    GPIO.digitalWrite(MotorB_speed,GPIO.LOW)
+    GPIO.digitalWrite(MotorB_dir,GPIO.LOW)
     GPIO.cleanup()
